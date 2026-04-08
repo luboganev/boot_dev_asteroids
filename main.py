4be4ce8fd8128b821981460d08226078ae253892
Lyubomir Ganev <luboganev@gmail.com>
2026-04-08 12:44:38 +0200

    add player drawing and some boilerplate

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -13,11 +14,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     gameloop_clock = pygame.time.Clock()
     delta_seconds = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
             pass
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         delta_seconds = gameloop_clock.tick(60) / 1000
 
