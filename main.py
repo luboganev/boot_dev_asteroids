c901935bbdbc53643a753f3318c104e919479a95
Lyubomir Ganev <luboganev@gmail.com>
2026-04-08 11:49:17 +0200

    add game clock and draw screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
+from logger import log_state
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,7 +9,17 @@ def main():
         f"Screen width: {SCREEN_WIDTH}\n" +
         f"Screen height: {SCREEN_HEIGHT}"
     )
-
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    gameloop_clock = pygame.time.Clock()
+    delta_seconds = 0
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            pass
+        screen.fill("black")
+        pygame.display.flip()
+        delta_seconds = gameloop_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
